6cfc1a33f2e735085d76ec3c52cf0a49cb8e91a1
hucguyue <18340076399@163.com>
2016-03-02 19:18:27 +0800

    本来窗体的bgcolor是黄色到红色的渐变，结果没出现。。。。。还以为哪里出错了呢。。。调试了半天。。。 还有就是调用finish()方法来结束欢迎界面，否则会出现按返回键会回到欢迎界面的错误。

--- src/com/example/myapp/WelcomeActivity.java
+++ src/com/example/myapp/WelcomeActivity.java
@@ -5,9 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
 public class WelcomeActivity extends Activity {
@@ -19,12 +16,15 @@ public class WelcomeActivity extends Activity {
 			// TODO Auto-generated method stub
 			switch (msg.what) {
 			case 1:
-				tv_timer.setText(msg.obj.toString());
+				tv_timer.setText(String.valueOf(msg.arg1));
+				// System.out.println("case1----"+msg.arg1);
+				System.out.println("case1----" + tv_timer.getText());
 				break;
 			case 2:
 				Intent intent = new Intent();
 				intent.setClass(WelcomeActivity.this, MainActivity.class);
 				startActivity(intent);
+				finish();// 结束欢迎页面
 				break;
 
 			default:
@@ -36,27 +36,18 @@ public class WelcomeActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		System.out.println("====concreate start");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
 		initView();
-		System.out.println("====concreate end");
 	}
-	@Override
-	protected void onStart() {
-		System.out.println("====constart start");
-		// TODO Auto-generated method stub
-		super.onStart();
-		System.out.println("====start e");
-	}
+
 	private void initView() {
 		// TODO Auto-generated method stub
-		tv_timer=(TextView) findViewById(R.id.wel_tv_timer);
+		tv_timer = (TextView) findViewById(R.id.wel_tv_timer);
 	}
 
 	@Override
 	protected void onResume() {
-		System.out.println("====resume start");
 		// TODO Auto-generated method stub
 		super.onResume();
 		Thread t = new Thread(new Runnable() {
@@ -65,16 +56,17 @@ public class WelcomeActivity extends Activity {
 			public void run() {
 				// TODO Auto-generated method stub
 				// 开始定时
-				for (int i = 4; i > 0; i--) {
+				for (int i = 5; i > 0; i--) {
 					try {
 						Thread.sleep(1000);
+						// System.out.println("======"+i);
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 					Message msg = new Message();
 					msg.what = 1;
-					msg.obj = i;
+					msg.arg1 = i;
 					handler.sendMessage(msg);
 				}
 				handler.sendEmptyMessage(2);
