72e7600d3bec2dddaab3049d8707047430b7d1a7
hucguyue <Jamesliu@James>
2016-03-07 21:10:15 +0800

    已经乱成一团麻，终于知道为啥不能用假数据了，好烦啊，是时候好好封装一下了。

--- src/com/example/myapp/UserInforActivity.java
+++ src/com/example/myapp/UserInforActivity.java
@@ -5,16 +5,26 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
+/**
+ * @author James
+ * @2016年3月3日上午10:49:28
+ */
 public class UserInforActivity extends Activity {
 	private TextView tv_userInfor;
 	private TextView tv_uititle;
@@ -30,6 +40,7 @@ public class UserInforActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_user_infor);
 		initView();
+		initClickEvent();
 		intent = getIntent();
 		username = intent.getStringExtra("username");
 		password = intent.getStringExtra("password");
@@ -44,7 +55,55 @@ public class UserInforActivity extends Activity {
 		}
 		userinfor.clear();
 		userinfor.addAll(d);
-		adapter.notifyDataSetChanged();
+		adapter.notifyDataSetChanged();// 唤醒适配器的数据交换
+	}
+
+	private void initClickEvent() {
+		// TODO Auto-generated method stub
+		this.lv_data
+				.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+					/**
+					 * (non-Javadoc) id: 被单击的项的行标识
+					 * 
+					 * @TODO点击每一项时出现对话框显示详细的信息。 </br>
+					 *                          <p>
+					 *                          1.自定义对话框</br>
+					 *                          >>创建布局解析器inflater</br>
+					 *                          >>解析某个布局文件
+					 *                          ，形成视图。inflate()的第二个参数表示生成视图的父视图
+					 *                          。</br> 2.往对话框中传递数据</br>
+					 * 
+					 *                          </p>
+					 * 
+					 * @see android.widget.AdapterView.OnItemClickListener#onItemClick(android.widget.AdapterView,
+					 *      android.view.View, int, long)
+					 */
+					@Override
+					public void onItemClick(AdapterView<?> parent, View view,
+							int position, long id) {
+						LayoutInflater inflater = LayoutInflater
+								.from(UserInforActivity.this);// 从当前的活动中创建一个布局解析器
+						View v = inflater.inflate(R.layout.alert_userinfor_all,
+								null);//
+
+						TextView tv_name = (TextView) findViewById(R.id.dialoguserinfor_tv_name);// 对item中的各个控件进行绑定
+						TextView tv_pwd = (TextView) findViewById(R.id.dialoguserinfor_tv_password);
+						TextView tv_pwdagain = (TextView) findViewById(R.id.dialoguserinfor_tv_pwdagain);
+						AlertDialog.Builder alert = new Builder(
+								UserInforActivity.this);// 创建一个对话框的对象
+						alert.setView(v);// 绑定该对话框的视图
+						alert.setPositiveButton("确定", null);
+						alert.setTitle("详细信息");
+						alert.setIcon(android.R.drawable.ic_menu_info_details);
+					/*	System.out.println("------->"
+								+ intent.getStringExtra("password"));
+						tv_name.setText(intent.getStringExtra("username"));
+						tv_pwd.setText(intent.getStringExtra("password"));
+						tv_pwdagain.setText(intent.getStringExtra("password"));*/
+						alert.show();
+					}
+				});
 	}
 
 	/**
